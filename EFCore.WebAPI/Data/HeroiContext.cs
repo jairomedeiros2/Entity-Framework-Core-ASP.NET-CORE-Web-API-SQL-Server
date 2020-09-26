0619a2ab4be8c8f1e0a02c42cf5d12ddb61609e0
jairo de melo medeiros <jairomedeiros2@hotmail.com>
2020-09-25 23:47:23 -0300

    Projeto realizado mediante aula do canal 'Programada Mente" abordando o assunto: Entity Framework Core + ASP.NET CORE Web API + SQL Server.
    - 01:41 - Criando o Projeto
    - 03:28 - Models
    - 07:32 - Contexto e Referências Nuget
    - 13:30 - Entidades do Contexto
    - 16:49 - String de Conexão
    - 19:31 - Migration e Update Database
    - 28:20 - Banco Criado
    - 31:56 - Script DB e Drop DB
    - 38:07 - Muitos pra Muitos
    - 45:04 - Um pra um
    - 50:44 - Solução: Erro Chave Composta
    - 59:44 - Scaffold - Do banco para Model
    - 01:08:45 - Enviando ao GIT
    - 01:10:10 - Curso Udemy
    
    Link do Curso: https://www.youtube.com/watch?v=ipbSwv09dDU

---
+++ EFCore.WebAPI/Data/HeroiContext.cs
@@ -0,0 +1,27 @@
+﻿using EFCore.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.WebAPI.Data
+{
+    public class HeroiContext : DbContext
+    {
+        public DbSet<Heroi> Herois { get; set; }
+        public DbSet<Batalha> Batalhas { get; set; }
+        public DbSet<Arma> Armas { get; set; }
+        public DbSet<HeroiBatalha> HeroisBatalhas { get; set; }
+        public DbSet<IdentidadeSecreta> IdentidadesSecretas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Integrated Security=SSPI;Persist Security Info=False;User ID=Jairo;Initial Catalog=HeroApp;Data Source=DESKTOP-VGILOF7");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HeroiBatalha>(entity =>
+            {
+                entity.HasKey(e => new { e.BatalhaId, e.HeroiId });
+            });
+        }
+    }
+}
